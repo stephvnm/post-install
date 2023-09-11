79c627bb49f7bab06c188f1099140f809895a799
stephvnm <stephvnm@gmail.com>
2023-09-11 15:40:19 +0300

    Update post_instal.sh

--- post_instal.sh
+++ post_instal.sh
@@ -68,6 +68,7 @@ sudo update-grub; sudo update-initramfs -u
 echo
 echo "Installing picom..."
 echo
+# ----------- https://github.com/FT-Labs/picom.git -----------------
 sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
 sudo apt install  libpcre3-dev python3-pip libxcb-xinerama0-dev
 sudo pip3 install meson ninja
